1f6a63b8f82df7504da0509b2a68bf1cb37bd57e
ArticleArtGH <artemchernykh07@gmail.com>
2020-02-11 02:08:50 +0300

    Застрял на выводе одновременном двух массивов

--- Math_methods/DinamicProg_1/Program.cs
+++ Math_methods/DinamicProg_1/Program.cs
@@ -18,20 +18,55 @@ namespace DinamicProg_1
                         {4,4,6,7,8},
                         {1,4,6,9,1}},
  
-                B = {{1,8,9,8,7,8},
-                    {3,5,6,7,6,6},
-                    {2,2,4,5,4,3},
-                    {2,3,4,5,2,1},
-                    {2,4,3,2,9,9}};
+            B = {{1,8,9,8,7,8},
+                {3,5,6,7,6,6},
+                {2,2,4,5,4,3},
+                {2,3,4,5,2,1},
+                {2,4,3,2,9,9}};
 
-            for(int i = A.GetLength(0);i>0;i--)
+            int i = 0, j = 0;
+            Console.WriteLine("Массив A = \n");
+            for (i = A.GetLength(0)-1;i>-1;i--)
             {
-                for (int j = A.GetLength(1); j > 0; j--)
+                for (j = 0; j < A.GetLength(1); j++)
                 {
-                    Console.WriteLine("Массив A = " + A[i, j]);
-
+                    Console.Write(" "+A[i,j]);
                 }
-                Console.WriteLine("/t");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Массив B = \n");
+            for (i = B.GetLength(0) - 1; i > -1; i--)
+            {
+                for (j = 0; j < B.GetLength(1); j++)
+                {
+                    Console.Write(" " + B[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            //Определени max i и j
+            int getLength_i = A.GetLength(0) > B.GetLength(0) ? A.GetLength(0) : B.GetLength(0);
+            int getLength_j = A.GetLength(1) > B.GetLength(1) ? A.GetLength(1) : B.GetLength(1);
+            Console.WriteLine("Массив A и B = \n");
+            for (i = getLength_i - 1; i > -1; i--)
+            {
+                for (j = 0; j < getLength_j; j++)
+                {
+                    if(A.GetLength(0)> i && A.GetLength(1) > j)
+                    {
+                        Console.Write("  " + A[i, j]);
+                    }
+                }
+                Console.WriteLine();
+                for (j = 0; j < getLength_j; j++)
+                {
+                    if (B.GetLength(0) > i && B.GetLength(1) > j)
+                    {
+                        Console.Write(" " + B[i, j]);
+                    }
+                }
+                Console.WriteLine();
             }
 
             Console.ReadKey(true);
