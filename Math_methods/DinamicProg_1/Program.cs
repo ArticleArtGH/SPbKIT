4f1bc5fa5abfb85e9c15212e1960371dcc2fadc0
ArticleArtGH <56726186+ArticleArtGH@users.noreply.github.com>
2020-02-12 01:17:30 +0300

    Add files via upload

--- Math_methods/DinamicProg_1/Program.cs
+++ Math_methods/DinamicProg_1/Program.cs
@@ -17,12 +17,14 @@ namespace DinamicProg_1
                         {3,5,7,9,8},
                         {4,4,6,7,8},
                         {1,4,6,9,1}},
- 
+
             B = {{1,8,9,8,7,8},
                 {3,5,6,7,6,6},
                 {2,2,4,5,4,3},
                 {2,3,4,5,2,1},
-                {2,4,3,2,9,9}};
+                {2,4,3,2,9,9}},
+
+            C = new int[6,6];
 
             int i = 0, j = 0;
             Console.WriteLine("Массив A = \n");
@@ -67,6 +69,14 @@ namespace DinamicProg_1
                     }
                 }
                 Console.WriteLine();
+
+                for (i=5;i>-1;i--)
+                {
+                    for(j = 5; j > -1; i--)
+                    {
+                        C[i, j] = (i == 5 && j == 5) ? 0 :  ;//!!! C[i,j]=C[i,j]+C[i,j-1]
+                    }
+                }
             }
 
             Console.ReadKey(true);
