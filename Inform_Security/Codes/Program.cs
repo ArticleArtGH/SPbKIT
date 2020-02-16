265814f195d03811d1edb9162de104454606f771
ArticleArtGH <artemchernykh07@gmail.com>
2020-02-17 02:14:47 +0300

    XOR

--- Inform_Security/Codes/Program.cs
+++ Inform_Security/Codes/Program.cs
@@ -17,21 +17,21 @@ namespace Caesars_code//Работает только для EN символов
             char[] c_array_alhabet = new char[alhabet.Length];
             c_array_alhabet = alhabet.ToCharArray();
 
-            WriteLine("Придумайте пароль на английском:");
+            WriteLine("Придумайте пароль на английском (только для букв малёнького регистра):");
             password = ReadLine();
             long_pswd = password.Length;
-            char[] c_arr_password = new char[long_pswd];
-            c_arr_password = password.ToCharArray();
+            char[] c_arr_password = password.ToCharArray(); 
 
-            WriteLine("Выберите задание по шифрованию \n" +
+            WriteLine("Выберите шифрованию \n" +
                 "1 - Цезарь\n" +
                 "2 - Вижлер\n" +
-                "3 - ...");
+                "3 - Перестановка\n"+
+                "4 - XOR\n");
             exercise = ReadLine(); 
             switch (exercise)
             {
                 case "1":
-                    WriteLine("Введите ключ для шифрования пароля:");
+                    WriteLine("Введите кодовое число (ключ) для шифрования пароля:");
                     key = Convert.ToInt32(ReadLine());
                     int index = 0, inkey = key;
                     for (int i = 0; i < long_pswd; i++)
@@ -53,9 +53,9 @@ namespace Caesars_code//Работает только для EN символов
                     }//ЗАШИФРОВКА пароля
                     WriteLine("Зашифрованный пароль:\n" + code);
 
-                    WriteLine("Введите пароль на английском, который хотите расшифровать:");
+                    WriteLine("Введите зашифрованный пароль, который хотите расшифровать:");
                     code = ReadLine();
-                    WriteLine("Введите ключ для рассшифрования пароля:");
+                    WriteLine("Введите число (ключ) для рассшифрования пароля:");
                     key = Convert.ToInt32(ReadLine());
                     long_pswd = code.Length;
                     c_arr_password = new char[long_pswd];
@@ -113,7 +113,7 @@ namespace Caesars_code//Работает только для EN символов
                         changePasword += s_key[k];
                         k++;
                     }
-                    WriteLine("Изменённый пароль для шифрования по Vigenere\n" + changePasword);
+                    WriteLine("Распределение ключа по длинные пароля для шифрования по Vigenere\n" + changePasword);
                     int index_I = 0, index_J = 0;
                     for (k = 0; k < long_pswd; k++)
                     {
@@ -123,11 +123,11 @@ namespace Caesars_code//Работает только для EN символов
                     }
                     WriteLine("Зашифрованный пароль:\n"+code);
 
-                    WriteLine("Введите пароль на английском, который хотите расшифровать:");
+                    WriteLine("Введите зашифрованный пароль, который хотите расшифровать:");
                     code = ReadLine();
                     WriteLine("Введите кодовое слово (ключ) для расшифрования пароля:");
                     s_key = ReadLine();
-                    k = 0; long_pswd = code.Length;
+                    k = 0; long_pswd = code.Length; changePasword = "";
                     for (int i = 0; i < long_pswd; i++)
                     {
                         if (k == s_key.Length)
@@ -137,24 +137,95 @@ namespace Caesars_code//Работает только для EN символов
                         changePasword += s_key[k];
                         k++;
                     }
-                    for (k = 0; k < long_pswd; k++)
+                    WriteLine("Распределение ключа по длинные пароля для расшифрования по Vigenere\n" + changePasword);
+                    password = "";
+                    for(int j = 0; j<long_pswd; j++)
                     {
-                        index_I = Array.BinarySearch(c_array_alhabet, code[k]);
-                        index_J = Array.BinarySearch(c_array_alhabet, changePasword[k]);
-                        code += table_Vigenere[index_I, index_J];
+                        index_J = Array.BinarySearch(c_array_alhabet, changePasword[j]);
+                        int i = 0;
+                        while(table_Vigenere[i, index_J] != code[j])
+                            i++;//без скобок - "{}" можно выполнить всего одно действие(одна - ; после неё уже 
+                        //будет считатья другое действие), например здесь в loop одно действие и в if:
+                        //for (int i = 0; i < c_array_alhabet.Length; i++)
+                        //    if (table_Vigenere[i, index_J] == code[j])
+                        //        password += c_array_alhabet[i];
+                        password += c_array_alhabet[i];
                     }
                     WriteLine("Расшифрованный пароль:\n" + password);
-                    //foreach(int i in table_Vigenere)
-                    //{
-                    //    WriteLine(i);
-                    //    foreach(int j in table_Vigenere)
-                    //    {
-                    //        WriteLine(j);
-                    //    }
-                    //}
                     break;
                 case "3":
+                    WriteLine("Введите кодовое число (ключ) для шифрования пароля:");
+                    key = Convert.ToInt32(ReadLine());
+                    while(key>long_pswd)
+                    {
+                        key = key - long_pswd;
+                    }
+                    index = key-1; code = ""; char c_sym = ' ';
+                    c_arr_password = password.ToCharArray();
+                    while (index + 1 < long_pswd)
+                    {
+                        c_sym = c_arr_password[index];
+                        c_arr_password[index] = c_arr_password[index + 1];
+                        c_arr_password[index + 1] = c_sym;
+                        index += key;
+                    }
+                    for(int i = 0; i<long_pswd; i++)
+                    {
+                        code += c_arr_password[i];
+                    }
+                    WriteLine("Зашифрованный пароль:\n" + code);
 
+                    WriteLine("Введите зашифрованный пароль, который хотите расшифровать:");
+                    code = ReadLine();
+                    WriteLine("Введите число (ключ) для рассшифрования пароля:");
+                    key = Convert.ToInt32(ReadLine());
+                    while (key > long_pswd)
+                    {
+                        key = key - long_pswd;
+                    }
+                    index = key - 1; password = ""; c_sym = ' ';
+                    c_arr_password = code.ToCharArray();
+                    //Array.Reverse(c_arr_password);
+                    while (index + 1 < long_pswd)
+                    {
+                        c_sym = c_arr_password[index];
+                        c_arr_password[index] = c_arr_password[index + 1];
+                        c_arr_password[index + 1] = c_sym;
+                        index += key;
+                    }
+                    //Array.Reverse(c_arr_password); 
+                    for (int i = 0; i < long_pswd; i++)
+                    {
+                        password += c_arr_password[i];
+                    }
+                    WriteLine("Расшифрованный пароль:\n" + password);
+                    break;
+                case "4":
+                    WriteLine("Введите кодовое слово (ключ) для шифрования пароля:");
+                    s_key = ReadLine();
+                    k = 0; changePasword = "";
+                    for (int i = 0; i < long_pswd; i++)
+                    {
+                        if (k == s_key.Length)
+                        {
+                            k = 0;
+                        }
+                        changePasword += s_key[k];
+                        k++;
+                    }
+                    WriteLine("Распределение ключа по длинные пароля для шифрования\n" + changePasword);
+                    byte[] bits = new byte[0]; string s_sym = "";//c_sym = ' ';
+                    //так как это ascii  то можно и просто символы складывать ...
+                    for (int i = 0; i<long_pswd;i++)
+                    {
+                        s_sym = Convert.ToString(password[i]);
+                        bits = Encoding.ASCII.GetBytes(s_sym);
+                        //xor
+                        //не понял в какой кодировку именно я должен получить и при помощи кодировку
+                        //я получу сразу двойичный код ?
+                    }
+
+                    //password = "";
                     break;
                 default: WriteLine("Вы ввели не верное значение!\n" +
                     "Программа ЗАКРЫВАЕТСЯ !\n" +
